e156e59d398296e947eb7c7bfacead343d106fd6
Vishal Dharam <vishal.dharam20@pccoepune.org>
2024-10-22 13:31:58 +0530

    Passport middleware configured

--- src/app.js
+++ src/app.js
@@ -2,7 +2,9 @@ export const app = require('express')
 
 const passport = require('passport')
 
-
+app.use(passport(("JWTStrategy"),{
+    console.log("Passport middleware sucessfully configured.")
+}))
 
 app.listen(2000,()=> {
     console.log(`Server is running on PORT ${2000}`)
