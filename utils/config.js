0ef07e3d7cd3777f8144ad49f9e71c7c49bb0867
Vishal Dharam <vishal.dharam20@pccoepune.org>
2024-10-22 13:59:42 +0530

    Public key added

--- utils/config.js
+++ utils/config.js
@@ -4,3 +4,5 @@ export const MESSAGE = "Server is running on PORT : ";
 
 export const EMAIL = "vishal.dh@bezt.in"
 export const PASSWORD = "ennc nndj loir";
+
+export const PUBLIC_KEY = "ermn3uy873b7edbduhd873bne3782";
