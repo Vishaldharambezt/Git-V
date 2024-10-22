0f48bd7c5c645ff0a1c0c592ddaf159e30c83d36
Vishal <vishal.dh@bezt.in>
2024-10-22 17:13:38 +0530

    Added the cahce ip

--- utils/config.js
+++ utils/config.js
@@ -9,3 +9,6 @@ export const PUBLIC_KEY = "ermn3uy873b7edbduhd873bne3782";
 
 export const PRIVATE_KEY = "eynfdf874nf8n4848f8flsldfm88";
 export const PRIVATE1cc_KEY1 = "eynfdf874nf8n4848f8flsldfm88";
+
+
+export const Redis_Host = 172.23.32.01
