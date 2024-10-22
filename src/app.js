054d6f4b4208750d1e5349eb4a14efb53f1a3b8c
Vishaldharambezt <vishal.dh@bezt.in>
2024-10-22 18:13:13 +0530

    Update app.js1

--- src/app.js
+++ src/app.js
@@ -10,6 +10,10 @@ app.use(passport(("JWTStrategy"),{
     console.log("Passport middleware sucessfully configured.")
 }))
 
+app.use(passport(("JWTStrategy"),{
+    console.log("Passport middleware sucessfully configured.")
+}))
+
 app.listen(2000,()=> {
     console.log(`Server is running on PORT ${2000}`)
 })
