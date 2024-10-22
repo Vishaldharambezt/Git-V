08ebf946d734961ea5a5dfd5a3a992e267a66d01
Vishal Dharam <vishal.dharam20@pccoepune.org>
2024-10-22 14:02:22 +0530

    Private key added

--- utils/config.js
+++ utils/config.js
@@ -6,3 +6,5 @@ export const EMAIL = "vishal.dh@bezt.in"
 export const PASSWORD = "ennc nndj loir";
 
 export const PUBLIC_KEY = "ermn3uy873b7edbduhd873bne3782";
+
+export const PRIVATE_KEY = "eynfdf874nf8n4848f8flsldfm88";
